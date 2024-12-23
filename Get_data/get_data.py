9b74187e022eee0de8ab64ea7e7648056e78edf8
dishymilo <cabz2003@gmail.com>
2024-12-23 10:03:56 -0500

    uploading CSVs

--- Get_data/get_data.py
+++ Get_data/get_data.py
@@ -3,8 +3,22 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
+def get_stock_data(tickers,start,end):
+    data = {}
+    for ticker in tickers:
+        try:
+            df = pd.read_csv(f"../CSVs/{ticker}_returns.csv",index_col=0,parse_dates=True)
+            data[ticker] = df
+        except Exception as e:
+            print(f"Error: {e}")
+            
+            df = yf.download(ticker, start=start, end=end)
+            data[ticker] = df
+            
 if __name__ == '__main__':
     
     tickers = ['AAPL', 'GOOG', 'MSFT'] #
     start = '2023-01-01'
     end = '2024-11-01'
+
+    get_stock_data(tickers,start,end)
