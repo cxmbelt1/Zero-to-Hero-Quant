4330dde639a490390d494e6218bc21275ad1f82f
dishymilo <cabz2003@gmail.com>
2024-12-23 10:51:44 -0500

    Get_data Tasks done

--- Get_data/get_data.py
+++ Get_data/get_data.py
@@ -1,3 +1,4 @@
+import os
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -5,20 +6,46 @@ import plotly.graph_objects as go
 
 def get_stock_data(tickers,start,end):
     data = {}
+    output_dir = os.path.join(os.getcwd(), "CSVs")
     for ticker in tickers:
         try:
-            df = pd.read_csv(f"../CSVs/{ticker}_returns.csv",index_col=0,parse_dates=True)
+
+            file_path = os.path.join(output_dir, f"{ticker}_returns.csv")
+            df = pd.read_csv(file_path,index_col=0,parse_dates=True)
             data[ticker] = df
         except Exception as e:
             print(f"Error: {e}")
             
             df = yf.download(ticker, start=start, end=end)
             data[ticker] = df
-            
+            df.to_csv(file_path)
+    
+    return data
+
+def plot(data,ticker):
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Candlestick(
+        x=data.index,open=data['Open'],high=data['High'],
+        low=data['Low'],close=data['Close'],name="Price"
+        ))
+
+    fig.update_layout(
+        title=f"{ticker} Stock Analysis",
+        xaxis_title="Date",
+        yaxis_title="Price",
+        xaxis=dict(rangeslider=dict(visible=False))
+    )
+
+    fig.show()
+
 if __name__ == '__main__':
     
     tickers = ['AAPL', 'GOOG', 'MSFT'] #
     start = '2023-01-01'
     end = '2024-11-01'
 
-    get_stock_data(tickers,start,end)
+    stock_data = get_stock_data(tickers=tickers,start=start,end=end)
+    for ticker,data in stock_data.items():
+        plot(data,ticker)
