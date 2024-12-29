e6c4ccd072633582e3acbbb8bf34f551d22959b7
cxmbelt1 <cabz2003@gmail.com>
2024-12-28 21:23:36 -0500

    actualizacion de ruta de busqueda de csv ya con linux, te amo linux

--- Binomial_Model/binomial_pricing_model.py
+++ Binomial_Model/binomial_pricing_model.py
@@ -1,36 +1,27 @@
-import os
 import yfinance as yf
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-def get_stock_data(tickers,start,end):
+def get_stock_data(tickers, start, end):
     data = {}
-    output_dir = os.path.join(os.getcwd(), "CSVs")
     for ticker in tickers:
         try:
-
-            file_path = os.path.join(output_dir, f"{ticker}_returns.csv")
-            df = pd.read_csv(file_path,index_col=0,parse_dates=True)
+            df = pd.read_csv(f'../CSVs/{ticker}_returns.csv', index_col=0, parse_dates=True)
             data[ticker] = df
         except Exception as e:
-            print(f"Error: {e}")
-            
+            print(f"Downloading data for {ticker} due to error: {e}")
             df = yf.download(ticker, start=start, end=end)
             data[ticker] = df
-            df.to_csv(file_path)
-    
+            df.to_csv(f'../CSVs/{ticker}_returns.csv')
     return data
 
 def calculate_volatility(data):
-    data["log_returns"] = np.log(data["Close"]/data["Close"].shift(1))
+    data['log_returns'] = np.log(data['Close'] / data['Close'].shift(1))
 
-    data["Rolling_Std"] = data["log_returns"].rolling(window=20).std()
-    data["Annualized_Vol"] = data["Rolling_Std"] * np.sqrt(252)
-    
-    data =data.dropna()
+    data['Rolling_Std'] = data['log_returns'].rolling(window=20).std()
+    data['Annualized_Vol'] = data['Rolling_Std'] * np.sqrt(252)
     return data
 
 def binomial_option_price(S, K, T, r, sigma, n, option_type="call"):
@@ -90,29 +81,28 @@ def calculate_option_price_binomial(data, risk_free_rate=0.03, days_to_expiratio
     
     return data
 
-
-def plot_with_options(data,ticker):
-
+def plot_with_options(data, ticker):
     fig = make_subplots(
-        rows=3, cols=1, shared_xaxes=True,
+        rows=3, cols=1, shared_xaxes=True, 
         vertical_spacing=0.02,
         row_heights=[0.6, 0.2, 0.2],
-        subplot_titles=("Candlestick Chart", "Call premium", "Put premium")
+        subplot_titles=("Candlestick Chart", "Calls", "Puts")
     )
-
-    fig.add_trace(go.Candlestick(
-        x = data.index, open=data["Open"], high=data["High"],
-        low=data["Low"], close=data["Close"], name="Price"
-        ), row=1, col=1)
-
-    fig.add_trace(go.Scatter(x=data.index, y=data["Call_Price"], 
-                             mode='lines', name='Call Price', line=dict(color="green")), row=2, col=1)
     
-    fig.add_trace(go.Scatter(x=data.index, y=data["Put_Price"], 
-                             mode='lines', name='Put Price', line=dict(color="red")), row=3, col=1)    
-
+    # Candlestick chart
+    fig.add_trace(go.Candlestick(
+        x=data.index, open=data['Open'], high=data['High'],
+        low=data['Low'], close=data['Close'], name='Price'
+    ), row=1, col=1)
+    
+    # Calls
+    fig.add_trace(go.Scatter(x=data.index, y=data['Call_Price'], mode='lines', name='Call_Price', line=dict(color='green')), row=2, col=1)
+    
+    # Puts
+    fig.add_trace(go.Scatter(x=data.index, y=data['Put_Price'], mode='lines', name='Put_Price', line=dict(color='red')), row=3, col=1)
+    
     fig.update_layout(
-        title = f"{ticker} Stock Analysis",
+        title=f"{ticker} Stock Analysis",
         xaxis=dict(rangeslider=dict(visible=False)),
         xaxis3_title="Date",
         yaxis1_title="Price",
@@ -124,16 +114,14 @@ def plot_with_options(data,ticker):
     
     fig.show()
 
-
 if __name__ == '__main__':
-    
     tickers = ['AAPL']
     start_date = '2023-01-01'
     end_date = '2023-12-31'
+    
+    stock_data = get_stock_data(tickers, start=start_date, end=end_date)
 
-    stock_data = get_stock_data(tickers=tickers,start=start_date,end=end_date)
-    for ticker,data in stock_data.items():
+    for ticker, data in stock_data.items():
         data = calculate_volatility(data)
         data = calculate_option_price_binomial(data, risk_free_rate=0.03, days_to_expiration=5)
-        
-        plot_with_options(data,ticker)
+        plot_with_options(data, ticker)
